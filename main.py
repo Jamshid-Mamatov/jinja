07eebf3b90aac8d4a9f23a3a6f89474277cdc7c0
Jamshid <mamatovjamshid506@gmail.com>
2021-03-01 19:39:21 +0500

    html template

--- main.py
+++ main.py
@@ -2,32 +2,62 @@ from flask import Flask
 
 app=Flask(__name__)
 
-html_home="<h1>HTML HOME</h1>"
 
 @app.route("/")
 def home():
-    link="""
+    title="home page"
+    content="home"
+    html_temp=f"""
+    <head>
+        <title>{title}</title>
+    </head>
+    <body>
+    <h1>{content}</h1>
+    <a href='/'>home</a>
     <a href='/product'>product</a>
-    <a href='/about'>about</a>
+    <a href='/about'>about</a>   
     <a href='/help'>help</a>
-    <a href='/contact'>contact</a>
+    <a href='/contact'>contact</a>  
+    </body>
+    </html>
     """
+    return html_temp
 
-    return link
 
 @app.route("/product")
 def product():
-    return "salom"
+    title="product page"
+    content="product"
+    html_temp=f"""
+    <head>
+        <title>{title}</title>
+    </head>
+    <body>
+    <h1>{content}</h1>
+    <a href='/'>home</a>
+    <a href='/product'>product</a>
+    <a href='/about'>about</a>   
+    <a href='/help'>help</a>
+    <a href='/contact'>contact</a>  
+    </body>
+    </html>
+    """
+    return html_temp
 
-@app.route("/help")
-def help():
-    return html_home
 
-@app.route("/about")
-def about():
-    return html_home
+# @app.route("/help")
+# def help():
+#     return html_home
 
-@app.route("/contact")
-def contact():
-    return html_home
+
+# @app.route("/about")
+# def about():
+#     return html_home
+
+
+# @app.route("/contact")
+# def contact():
+#     return html_home
+
+
 app.run(debug=True)
