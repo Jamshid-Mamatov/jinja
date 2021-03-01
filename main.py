26fc9d0d9b0e30477ea099f181baa0d1c0a56e66
Jamshid <mamatovjamshid506@gmail.com>
2021-03-01 21:51:30 +0500

    templata page

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from flask import Flask
-
+from jinja2 import Template
 app=Flask(__name__)
 
-
-@app.route("/")
-def home():
-    title="home page"
-    content="home"
-    html_temp=f"""
+html_temp=Template("""
     <head>
-        <title>{title}</title>
+        <title>{{title}}</title>
     </head>
     <body>
-    <h1>{content}</h1>
+    <h1>{{content}}</h1>
     <a href='/'>home</a>
     <a href='/product'>product</a>
     <a href='/about'>about</a>   
@@ -20,44 +15,43 @@ def home():
     <a href='/contact'>contact</a>  
     </body>
     </html>
-    """
-    return html_temp
+    """)
+
+@app.route("/")
+def home():
+    title="home page"
+    content="home"
+    
+    return html_temp.render(title=title,content=content)
 
 
 @app.route("/product")
 def product():
     title="product page"
     content="product"
-    html_temp=f"""
-    <head>
-        <title>{title}</title>
-    </head>
-    <body>
-    <h1>{content}</h1>
-    <a href='/'>home</a>
-    <a href='/product'>product</a>
-    <a href='/about'>about</a>   
-    <a href='/help'>help</a>
-    <a href='/contact'>contact</a>  
-    </body>
-    </html>
-    """
-    return html_temp
+    
+    return html_temp.render(title=title,content=content)
 
 
-# @app.route("/help")
-# def help():
-#     return html_home
+@app.route("/help")
+def help():
+    title="help page"
+    content="help"
+    return html_temp.render(title=title,content=content)
 
 
-# @app.route("/about")
-# def about():
-#     return html_home
+@app.route("/about")
+def about():
+    title="about page"
+    content="about"
+    return html_temp.render(title=title,content=content)
 
 
-# @app.route("/contact")
-# def contact():
-#     return html_home
+@app.route("/contact")
+def contact():
+    title="content page"
+    content="content"
+    return html_temp.render(title=title,content=content)
 
 
 app.run(debug=True)
