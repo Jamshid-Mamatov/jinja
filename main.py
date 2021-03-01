bdb0ff7a48c15692ea85b1f47dac801eef57db7b
Jamshid <mamatovjamshid506@gmail.com>
2021-03-01 18:38:23 +0500

    pages

--- main.py
+++ main.py
@@ -6,7 +6,21 @@ html_home="<h1>HTML HOME</h1>"
 
 @app.route("/")
 def home():
-
     return html_home
 
+@app.route("/product")
+def product():
+    return html_home
+
+@app.route("/help")
+def help():
+    return html_home
+
+@app.route("/about")
+def about():
+    return html_home
+
+@app.route("/contact")
+def contact():
+    return html_home
 app.run(debug=True)
